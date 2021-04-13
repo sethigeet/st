4237d4b5f9b5fe336def02908a55cad0ee74847f
Geet Sethi <geetsethi05@gmail.com>
2021-04-13 17:15:50 +0530

    :construction: Added transparency to the terminal

--- config.def.h
+++ config.def.h
@@ -97,6 +97,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* Default color scheme (Ayu mirage) */
@@ -134,7 +137,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
